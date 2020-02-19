8797f44595e9387023453f6500234c6ef98dbc8c
Craig J. Bass <craig@madetech.com>
2020-02-19 18:48:44 +0000

    Refactor events into it's own class.

---
+++ PrimeOrdinance/Events.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PrimeOrdinance
+{
+    public class Events
+    {
+        private readonly List<Event> _events = new List<Event>();
+        public class Event
+        {
+            public Guid Id;
+            public string Type;
+            public object Data;
+        }
+
+        public IEnumerable<Event> All => _events.AsReadOnly().AsEnumerable();
+
+        public IEnumerable<Event> AllInReverse => _events.AsReadOnly().Reverse();
+
+        public Guid EmitGameEvent(string eventType, object data)
+        {
+            var id = Guid.NewGuid();
+            var @event = new Event
+            {
+                Id = id,
+                Type = eventType,
+                Data = data
+            };
+            _events.Add(@event);
+            return id;
+        }
+    }
+}
--- PrimeOrdinance/Game.cs
+++ PrimeOrdinance/Game.cs
@@ -6,7 +6,68 @@ namespace PrimeOrdinance
 {
     public class Game
     {
-        private readonly List<Event> _events = new List<Event>();
+        private const string EventAddPlayer = "add-player";
+        private const string EventBuildUnit = "build-unit";
+        private const string EventDestroyUnit = "destroy-unit";
+        
+        private Events _events;
+
+        public Game()
+        {
+            _events = new Events();
+        }
+
+        public PresentableLobby ShowLobby()
+        {
+            return new PresentableLobby
+            {
+                Players = _events.All
+                    .Where(e => e.Type == EventAddPlayer)
+                    .Select(e => (string) e.Data)
+                    .ToArray()
+            };
+        }
+
+        public void AddPlayer(string playerName)
+        {
+            _events.EmitGameEvent(EventAddPlayer, playerName);
+        }
+
+        public string BuildUnit(string unitType)
+        {
+            var id = _events.EmitGameEvent(EventBuildUnit, unitType);
+            return id.ToString();
+        }
+
+        public void DestroyUnit(string id)
+        {
+            _events.EmitGameEvent(EventDestroyUnit, Guid.Parse(id));
+        }
+
+        public PresentableUnits ViewUnits()
+        {
+            var units = new List<string>();
+            var destroyed = new List<Guid>();
+
+            foreach (var @event in _events.AllInReverse)
+            {
+                if (@event.Type == EventDestroyUnit)
+                {
+                    destroyed.Add((Guid) @event.Data);
+                    continue;
+                }
+
+                if (destroyed.Contains(@event.Id))
+                    continue;
+
+                if (@event.Type == EventBuildUnit) units.Add((string) @event.Data);
+            }
+
+            return new PresentableUnits()
+            {
+                Units = units.AsReadOnly().Reverse().ToArray()
+            };
+        }
 
         public class PresentableLobby
         {
@@ -17,78 +78,5 @@ namespace PrimeOrdinance
         {
             public string[] Units;
         }
-
-        private class Event
-        {
-            public Guid Id;
-            public string Type;
-            public object Data;
-        }
-
-        private Guid EmitGameEvent(string eventType, object data)
-        {
-            var id = Guid.NewGuid();
-            var @event = new Event
-            {
-                Id = id,
-                Type = eventType,
-                Data = data
-            };
-            _events.Add(@event);
-            return id;
-        }
-
-        public PresentableLobby ShowLobby()
-        {
-            return new PresentableLobby
-            {
-                Players = _events
-                    .Where(e => e.Type == "add-player")
-                    .Select(e => (string) e.Data)
-                    .ToArray()
-            };
-        }
-
-        public void AddPlayer(string playerName)
-        {
-            EmitGameEvent("add-player", playerName);
-        }
-
-        public string BuildUnit(string unitType)
-        {
-            var id = EmitGameEvent("build-unit", unitType);
-            return id.ToString();
-        }
-
-        public void DestroyUnit(string id)
-        {
-            EmitGameEvent("destroy-unit", Guid.Parse(id));
-        }
-
-        public PresentableUnits ViewUnits()
-        {
-            var units = new List<string>();
-            var destroyed = new List<Guid>();
-            
-            foreach (var @event in _events.AsReadOnly().Reverse())
-            {
-                if (@event.Type == "destroy-unit")
-                {
-                    destroyed.Add((Guid) @event.Data);
-                    continue;
-                }
-
-                if (destroyed.Contains(@event.Id))
-                    continue;
-
-                if (@event.Type == "build-unit") units.Add((string) @event.Data);
-            }
-            
-            return new PresentableUnits()
-            {
-                Units = units.AsReadOnly().Reverse().ToArray()
-            };
-        }
     }
-
 }
