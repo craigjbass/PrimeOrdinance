c77affeaa600aadb131824eaf1bf66dd79bfb751
Craig J. Bass <1889973+craigjbass@users.noreply.github.com>
2020-02-19 23:43:18 +0000

    Protobuf spike

---
+++ PrimeOrdinance.ConsoleClient/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using PrimeOrdinance.Protocols;
+
+namespace PrimeOrdinance.ConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Channel channel = new Channel("127.0.0.1:50051", ChannelCredentials.Insecure);
+
+            var client = new Lobby.LobbyClient(channel);
+
+            var reply = client.ViewLobby(new Empty());
+            Console.WriteLine("Greeting: " + reply.PlayerNames);
+
+            channel.ShutdownAsync().Wait();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PrimeOrdinance.Server/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using PrimeOrdinance.Protocols;
+
+namespace PrimeOrdinance.Server
+{
+    class Program
+    {
+        const int Port = 50051;
+
+        static void Main(string[] args)
+        {
+            var game = new Game();
+            
+            Grpc.Core.Server server = new Grpc.Core.Server
+            {
+                Services = { Lobby.BindService(new LobbyImpl(game)) },
+                Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+            };
+            server.Start();
+
+            Console.WriteLine("Greeter server listening on port " + Port);
+            Console.WriteLine("Press any key to stop the server...");
+            Console.ReadKey();
+
+            server.ShutdownAsync().Wait();
+        }
+    }
+
+    internal class LobbyImpl : Lobby.LobbyBase
+    {
+        private readonly Game _game;
+
+        public LobbyImpl(Game game)
+        {
+            _game = game;
+        }
+
+        public override Task<PresentableLobby> ViewLobby(Empty request, ServerCallContext context)
+        {
+            var presentableLobby = new PresentableLobby();
+            presentableLobby.PlayerNames.AddRange(_game.ShowLobby().Players);
+            return Task.FromResult(presentableLobby);
+        }
+    }
+}
