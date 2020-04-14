d5befe870607cb44dfde9140c92dfb007cd5124e
Craig J. Bass <1889973+craigjbass@users.noreply.github.com>
2020-04-14 18:02:42 +0100

    Some artwork ideas

--- prime-ordinance-client/src/App.js
+++ prime-ordinance-client/src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const canvas = useRef(null);
+  useEffect(() => {
+    canvas.current.requestPointerLock();
+  });
+
   return (
-    <svg className="canvas">
-        <circle className="originator" cx="200" cy="200" r="4"/>
+    <svg ref={canvas} className="canvas">
+        <g>
+          <circle className="zone" cx="200" cy="200" r="100"/>
+          <circle className="originator" cx="200" cy="200" r="4">
+                <animate attributeName="r" values="4;6;4" dur="1s" repeatCount="indefinite" />
+                <animate attributeName="stroke-width" values="2;6;2" dur="1s" repeatCount="indefinite" />
+          </circle>
+        </g>
     </svg>
   );
 }
